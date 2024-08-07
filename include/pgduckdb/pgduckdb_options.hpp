616b485a34e765f9c812e33cf090785468d9c56a
mkaruza <mkaruza@users.noreply.github.com>
2024-08-07 10:24:29 +0200

    Local object cache for cached_httpfs extension
    * To cache remote object, user need to explicilty use
      `duckdb.cache(path, type)` function. Path is remote HTTPFS/S3/GCS/R2
      object path and type is either `parquet` or `csv` indicating remote
      object type. Caching will not be triggered on normal `SELECT` queries.
    
    * When file is currently being cached and if we run in parallel another
      query targeting same remote object - second query will fail because it
      will not be able to take read lock.
    
    * PG_DATADIR/duckdb_cache will be used as cache directory

--- include/pgduckdb/pgduckdb_options.hpp
+++ include/pgduckdb/pgduckdb_options.hpp
@@ -1,42 +1,19 @@
 #pragma once
 
+#include "duckdb.hpp"
+
 #include <string>
 #include <vector>
 
 namespace pgduckdb {
 
-/* constants for duckdb.secrets */
-#define Natts_duckdb_secret              7
-#define Anum_duckdb_secret_type          1
-#define Anum_duckdb_secret_id            2
-#define Anum_duckdb_secret_secret        3
-#define Anum_duckdb_secret_region        4
-#define Anum_duckdb_secret_endpoint      5
-#define Anum_duckdb_secret_r2_account_id 6
-#define Anum_duckdb_secret_use_ssl       7
+void AddSecretsToDuckdbContext(duckdb::ClientContext &context);
 
-typedef struct DuckdbSecret {
-	std::string type;
-	std::string id;
-	std::string secret;
-	std::string region;
-	std::string endpoint;
-	std::string r2_account_id;
-	bool		use_ssl;
-} DuckdbSecret;
-
-extern std::vector<DuckdbSecret> ReadDuckdbSecrets();
-
-/* constants for duckdb.extensions */
-#define Natts_duckdb_extension       2
-#define Anum_duckdb_extension_name   1
-#define Anum_duckdb_extension_enable 2
-
-typedef struct DuckdbExension {
+typedef struct DuckdbExtension {
 	std::string name;
 	bool enabled;
-} DuckdbExension;
+} DuckdbExtension;
 
-extern std::vector<DuckdbExension> ReadDuckdbExtensions();
+extern std::vector<DuckdbExtension> ReadDuckdbExtensions();
 
 } // namespace pgduckdb
